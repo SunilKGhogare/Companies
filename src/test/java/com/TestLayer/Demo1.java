dc5c19fcd9c381eefea8d5be25f2dccbcebd8967
Sunil Ghogare <sunilghogare377@gmail.com>
2023-10-23 14:33:54 +0530

    [Sunil] : new class is created

--- src/test/java/com/TestLayer/Demo1.java
+++ src/test/java/com/TestLayer/Demo1.java
@@ -2,9 +2,9 @@ package com.TestLayer;
 
 public class Demo1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		
 	}
 
 }
